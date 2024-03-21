3f905dd58df4f63f7536a85db81b30b8c8f99316
zomars <zomars@me.com>
2024-03-21 16:39:09 -0700

    fixes
    Signed-off-by: zomars <zomars@me.com>

--- src/repositories/tasks.ts
+++ src/repositories/tasks.ts
@@ -51,6 +51,9 @@ export class Task {
   static async getNextBatch() {
     const tasks = await db.task.findMany({
       where: upcomingTasksWhere,
+      orderBy: {
+        scheduledAt: "asc",
+      },
       take: 100,
     });
     return tasks;
